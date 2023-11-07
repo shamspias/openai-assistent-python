7315780cabe70adb4e772c77151d0b0d032430b4
shamspias <shamsuddinpias0@gmail.com>
2023-11-07 16:49:24 +0600

    fix: null file ids chat interfacte

--- chat_interface.py
+++ chat_interface.py
@@ -1,3 +1,5 @@
+# chat_interface.py
+
 class TerminalChatInterface:
     """A simple terminal interface to interact with an OpenAI Assistant."""
 
@@ -5,11 +7,12 @@ class TerminalChatInterface:
         """
         Initialize the interface with an instance of OpenAIAssistant.
         If both assistant ID and name are provided, verify they match.
+        If file IDs are provided, store them for use in creating threads.
         """
         self.assistant_manager = assistant_manager
         self.assistant_id = assistant_id
         self.assistant_name = assistant_name
-        self.file_ids = file_ids
+        self.file_ids = file_ids  # Store the provided file IDs
         self.thread_id = None
 
         if assistant_id and assistant_name:
@@ -32,22 +35,22 @@ class TerminalChatInterface:
             return
 
         print(f"Welcome to the OpenAI Chatbot Interface.")
-        print(f"You are now chatting with assistant ID '{self.assistant_id}'.")
+        if self.assistant_name:
+            print(f"You are now chatting with assistant '{self.assistant_name}' (ID: '{self.assistant_id}').")
+        else:
+            print(f"You are now chatting with assistant ID '{self.assistant_id}'.")
         self.start_thread()
 
     def start_thread(self):
         """Start a new thread for conversation with the assistant."""
+        initial_message = "Hello, how can I assist you today?"
         try:
-            initial_message = "Hello, how can I assist you today?"
-            thread = self.assistant_manager.create_thread(self.assistant_id, initial_message,
-                                                          file_ids=self.file_ids)  # Updated
+            thread = self.assistant_manager.create_thread(self.assistant_id, initial_message, file_ids=self.file_ids)
             self.thread_id = thread.get('id')
             print(f"Bot: {initial_message}")
-
-            # Chat loop
-            self.chat_loop()
+            self.chat_loop()  # Start the chat loop after initializing the thread
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred while creating a thread: {e}")
             exit()
 
     def chat_loop(self):
@@ -60,11 +63,15 @@ class TerminalChatInterface:
             elif user_message.startswith('/'):
                 self.handle_command(user_message)
             else:
-                try:
-                    response = self.assistant_manager.chat_with_assistant(self.thread_id, user_message)
-                    print(f"Bot: {response['choices'][0]['message']['content']}")
-                except Exception as e:
-                    print(f"An error occurred while sending the message: {e}")
+                self.send_message(user_message)
+
+    def send_message(self, message):
+        """Send a message to the assistant and handle the response."""
+        try:
+            response = self.assistant_manager.chat_with_assistant(self.thread_id, self.assistant_id, message)
+            print(f"Bot: {response['choices'][0]['message']['content']}")
+        except Exception as e:
+            print(f"An error occurred while sending the message: {e}")
 
     def handle_command(self, command):
         """Handle special commands in the chat interface."""
