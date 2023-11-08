5add579556f48cb6fe303caa28830fc9509cc38d
shamspias <shamsuddinpias0@gmail.com>
2023-11-08 12:53:56 +0600

    fix: main

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-# main.py
+import asyncio
 import os
 from dotenv import load_dotenv
-import openai
-from openai_assistant import OpenAIAssistant
-from chat_interface import TerminalChatInterface
+from openai._client import AsyncOpenAI
+from thread_manager import ThreadManager
+from assistant_manager import AssistantManager
+from chat_session import ChatSession
 
 # Load environment variables from .env file
 load_dotenv()
 
+# Use the environment variables
+API_KEY = os.getenv('API_KEY')
+ASSISTANT_NAME = os.getenv('ASSISTANT_NAME')
+ASSISTANT_ID = os.getenv('ASSISTANT_ID')
+MODEL_NAME = os.getenv('MODEL_NAME')
 
-def main():
-    api_key = os.getenv('OPENAI_API_KEY')
-    assistant_id = os.getenv('ASSISTANT_ID')
-    assistant_name = os.getenv('ASSISTANT_NAME')
-    file_ids = os.getenv('FILE_IDS', '').split(',') if os.getenv('FILE_IDS') else []
 
-    openai_client = openai.Client(api_key=api_key)
-    assistant_manager = OpenAIAssistant(openai_client)
-    chat_interface = TerminalChatInterface(
-        assistant_manager,
-        assistant_id=assistant_id,
-        assistant_name=assistant_name,
-    )
-    chat_interface.start_chat()
+# ASSISTANT_ID will be fetched dynamically after creation or during the session
+
+# Entry point
+async def main():
+    # Initialize the OpenAI async client
+    openai_async_client = AsyncOpenAI(api_key=API_KEY)
+
+    # Create instances of manager classes with the OpenAI client
+    thread_manager = ThreadManager(openai_async_client)
+    assistant_manager = AssistantManager(openai_async_client)
+
+    # Start a chat session with the assistant name and model name
+    chat_session = ChatSession(thread_manager, assistant_manager, ASSISTANT_NAME, MODEL_NAME, ASSISTANT_ID)
+    await chat_session.start_session()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
